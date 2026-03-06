4afc1756e9404540a9faf80bb33d244bdd2306d3
Eric Petersen <ericpetersen3.14@gmail.com>
2026-03-06 10:48:36 -0700

    98% accuracy is insane

--- model.py
+++ model.py
@@ -83,20 +83,20 @@ target_names = [
 
 model = models.Sequential()
 
-model.add(layers.Conv2D(8, (3, 3), activation='relu', input_shape=(100, 100, 3)))
+model.add(layers.Conv2D(16, (3, 3), activation='relu', input_shape=(100, 100, 3)))
 model.add(layers.BatchNormalization())
-model.add(layers.MaxPooling2D((2, 2)))
-
-model.add(layers.Conv2D(32, (3, 3), activation='relu'))
-model.add(layers.BatchNormalization())
-model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.MaxPooling2D((3, 3)))
 
 model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 model.add(layers.BatchNormalization())
-model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.MaxPooling2D((3, 3)))
+
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.BatchNormalization())
+model.add(layers.MaxPooling2D((3, 3)))
 
 model.add(layers.Flatten())
-model.add(layers.Dense(64, activation="relu"))
+model.add(layers.Dense(256, activation="relu"))
 model.add(layers.BatchNormalization())
 model.add(layers.Dense(43, activation="softmax"))
 model.summary()
